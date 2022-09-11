c29460c8abf1ce26b85f6d821848b995fd0869c0
Ahmad Azaam Qanadilo -X (aqanadil) <aqanadil@cisco.com>
2022-09-11 18:28:53 +0300

    adding booking to admin

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from base.models import FarmModel, FarmImageModel
+from base.models import FarmModel, FarmImageModel, BookingModel
 
 # Register your models here.
 admin.site.register(FarmModel)
 admin.site.register(FarmImageModel)
+admin.site.register(BookingModel)
